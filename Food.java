457b4e897c4f654e92b261a280ee12dcb33ff782
Matthew Helkey <mshelkey@ucdavis.edu>
2021-01-16 09:59:06 -0800

    finished Food.java skeleton

--- Food.java
+++ Food.java
@@ -3,35 +3,93 @@
 this is an OOP class 
 it will have food name, number, barcode, and pointer to an image of said food
 
+Food(int num, String name, int code, String imageName)
 
+void setBarCode(int code) //sets the barcode
+void setName(String name) //sets the name of fooditem
+void setNum(int num) //sets num of food items
+
+int returnNumItems(void)
+int returnBarCode(void)
+String returnItemName(void)
+String returnImageName(void)
+
+
+String printFood(void)
 
 
 */
 
-
+package Food;
 
 public class Food
 {
-private:
-	int numItems, barcode;
-	String itemName, imageName; //idk how else to store images in java yet
-public:
-	Food(int num, String name, int code, String imageName)
+
+	private int numItems;
+
+	private String itemName, imageName, barcode; //idk how else to store images in java yet
+
+	public Food(int num, String name, String code, String imageName)
 	{
-		numItems = num;
-		barcode = code;
+		setBarCode(code);
+		setName(name);
+
 
 	}
-	void setCode(int code)
+	public void setBarCode(String code) //sets the barcode
 	{
-		if(String.valueOf(code).length() == 12) //checks if barcode is 12 digits (which is how long barcodes are)
+		if(code.length() == 12) //checks if barcode is 12 digits (which is how long barcodes are)
 		{
 			barcode = code;
 		}
 		else
 		{
-			barcode = 111222333444;
+			barcode = "111222333444";
 		}
 
 	}
+	public void setName(String name) //sets name of food item
+	{
+		itemName = name;
+	}
+	public void setNum(int num) //sets num of food items
+	{
+		if(num < 0)
+		{
+			numItems = 0;
+		}
+		else
+		{
+			numItems = num;
+		}
+	}
+	public void setImageName(String name)
+	{
+		imageName = name;
+	}
+
+
+	public int returnNumItems()
+	{
+		return numItems;
+	}
+	public String returnBarCode()
+	{
+		return barcode;
+	}
+	public String returnItemName()
+	{
+		return itemName;
+	}
+	public String returnImageName()
+	{
+		return imageName;
+	}
+
+	public String printFood()
+	{
+		String temp = itemName + " Num: " + numItems + ". Barcode: " + barcode;
+		return temp;
+	}
+
 }
