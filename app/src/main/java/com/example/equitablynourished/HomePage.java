5c5329b79527541da0f32c6c962fdf21728d6138
adityaaravi <adityaaravi6@gmail.com>
2021-01-17 13:45:32 -0800

    Collect and donate view starting pages created

--- app/src/main/java/com/example/equitablynourished/HomePage.java
+++ app/src/main/java/com/example/equitablynourished/HomePage.java
@@ -2,7 +2,9 @@ package com.example.equitablynourished;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomePage extends AppCompatActivity {
 
@@ -10,5 +12,27 @@ public class HomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+    }
+
+    // method to be called when the 'donate button' is clicked.
+    public void openDonateView(View view){
+        Intent intent = new Intent(this, DonateView.class);
+        startActivity(intent);
+    }
+
+    //method to be called when the 'collect' button is clicked.
+    public void openCollectView(View view){
+        Intent intent = new Intent(this, CollectView.class);
+        startActivity(intent);
+    }
+
+    //method to be called when the 'history' button is clicked.
+    public void openHistoryView(View view){
+        // TO BE IMPLEMENTED LATER
+    }
+
+    //method to be called when the 'Account settings' button is clicked
+    public void openAccountSettings(View view){
+
     }
 }
