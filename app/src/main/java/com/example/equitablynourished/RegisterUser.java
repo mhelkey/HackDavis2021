7b6450ba914fe3795b9fef2fe17abd423131f6fc
Matthew Helkey <72168312+mhelkey@users.noreply.github.com>
2021-01-16 16:03:14 -0800

    no warnings or errors (at least from waht i was working on) when building and integrating firebase

--- app/src/main/java/com/example/equitablynourished/RegisterUser.java
+++ app/src/main/java/com/example/equitablynourished/RegisterUser.java
@@ -1,6 +1,6 @@
 package com.example.equitablynourished;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class RegisterUser extends AppCompatActivity {
