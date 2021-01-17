939b70d66cc72eb3d28a6491dc0ad1ec0e91fed1
adityaaravi <adityaaravi6@gmail.com>
2021-01-17 14:25:05 -0800

    Collector view information conformation page design done

--- app/src/main/java/com/example/equitablynourished/DonateView.java
+++ app/src/main/java/com/example/equitablynourished/DonateView.java
@@ -10,5 +10,11 @@ public class DonateView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_donate_view);
+        //get data and change the address label
     }
+
+    // method to call when the `correct` address button is pressed
+
+    //method to call when teh `wrong` address button is pressed
+
 }
