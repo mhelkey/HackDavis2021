ed8d6dfbe3cc5d5452c0fcfadef84a1c41067591
Matthew Helkey <72168312+mhelkey@users.noreply.github.com>
2021-01-17 15:50:40 -0800

    code to compule arraylist of foodbank data is complete

--- app/src/main/java/com/example/equitablynourished/CollectView.java
+++ app/src/main/java/com/example/equitablynourished/CollectView.java
@@ -13,6 +13,8 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+
 public class CollectView extends AppCompatActivity {
 
     @Override
@@ -28,6 +30,13 @@ public class CollectView extends AppCompatActivity {
                 String data = "";
                 // Get Post object and use the values to update the UI
                 data = dataSnapshot.getValue(String.class);
+
+                ArrayList<String> list = new ArrayList<String>();
+                list.add("ASUCD Pantry Num: 145. Barcode: Memorial Union, 1 Shields Ave #154, Davis, CA 956161525 State St, Santa Barbara, CA 93101");
+                list.add("Food Bank Num: 123456. Barcode: Earth");
+                list.add("Santa Barbara Food Bank Num: 1212. Barcode: 1525 State St, Santa Barbara, CA 93101");
+                list.add("Yolo Food Bank Num: 12. Barcode: 233 Harter Ave, Woodland, CA 95776");
+
                 // ...
             }
 
