48be7cd0ebefb167b282cbfc09b4076ce1ef6b90
Matthew Helkey <72168312+mhelkey@users.noreply.github.com>
2021-01-17 15:19:37 -0800

    Deleted the code i wrode in lieu of Adityaa's commit for CollectView

--- app/src/main/java/com/example/equitablynourished/CollectView.java
+++ app/src/main/java/com/example/equitablynourished/CollectView.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class CollectView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collect_view);
+
     }
 }
